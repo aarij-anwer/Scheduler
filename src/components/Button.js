44537d203f0b13f91f4765fc8f68237d9faa0c7c
Aarij Anwer <aarij.anwer@gmail.com>
2023-03-02 14:47:08 -0500

    add buttonClass to Button and enable unit tests

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 import "components/Button.scss";
+import classNames from "classnames";
+
 
 export default function Button(props) {
-   return <></>;
+
+   // console.log("Button props", props);
+  
+   const buttonClass = classNames("button", {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger
+    });
+
+    console.log("buttonClass", buttonClass);
+
+   return <button
+      className={buttonClass}
+      disabled={props.disabled}
+      onClick={props.onClick}
+   >
+      {props.children}
+   </button>
 }
