4068db897945d390994f3fba08bd08fcdff812ab
Aarij Anwer <aarij.anwer@gmail.com>
2023-03-03 01:39:40 -0500

    fix "spots" in DayListItem, passes all tests

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -18,10 +18,23 @@ export default function DayListItem(props) {
 
   console.log("listClass", listClass);
 
+  const spotsText = () => {
+    let answer = props.spots + " spots remaining";
+
+    if (props.spots === 0) {
+      answer = "no spots remaining";
+    } else if (props.spots === 1) {
+      answer = "1 spot remaining";
+    }
+
+    console.log("answer", answer);
+    return answer;
+  };
+
   return (
     <li className={listClass} onClick={() => props.setDay(props.name)} >
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{spotsText()}</h3>
     </li>
   );
 }
