4a3d1db1e0944ce4590f062fe169874ddd99bf50
Aarij Anwer <aarij.anwer@gmail.com>
2023-03-04 17:11:16 -0500

    fix DayListItem.scs to make the parent styles work

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import classNames from "classnames";
+import classNames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
@@ -7,14 +7,14 @@ export default function DayListItem(props) {
 
   const spots = props.spots === 0;
 
-  // const listClass = classNames("day-list__item", {
-  //   "--selected": props.selected,
-  //   "--full": spots
-  // });
+  const listClass = classNames("day-list__item", {
+    "--selected": props.selected,
+    "--full": spots
+  });
 
-  let listClass = "day-list__item";
-  (props.selected) ? listClass+="--selected" : listClass+="";
-  (spots) ? listClass+="--full" : listClass+=""
+  // let listClass = "day-list__item";
+  // (props.selected) ? listClass+="--selected" : listClass+="";
+  // (spots) ? listClass+="--full" : listClass+=""
 
   console.log("listClass", listClass);
 
