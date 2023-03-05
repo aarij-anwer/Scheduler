ca971a8612ba12118252c80ab2305cc3ec0f5b06
Aarij Anwer <aarij.anwer@gmail.com>
2023-03-05 02:06:14 -0500

    refactor InterviewList for the controlled list pattern

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -11,8 +11,8 @@ const InterviewerList = (props) => {
       key={interviewer.id}
       name={interviewer.name}
       avatar={interviewer.avatar}
-      selected={((props.interviewer === interviewer.id))}
-      setInterviewer={(event) => props.setInterviewer(interviewer.id)}
+      selected={((props.value === interviewer.id))}
+      setInterviewer={(event) => props.onChange(interviewer.id)}
     />
   );
 
--- stories/index.js
+++ stories/index.js
@@ -113,6 +113,7 @@ const interviewers = [
   { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
 ];
 
+
 storiesOf("InterviewerList", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -125,13 +126,12 @@ storiesOf("InterviewerList", module)
   .add("Selected", () => (
     <InterviewerList
       interviewers={interviewers}
-      interviewer={3}
+      value={3}
     />
   ))
   .add("Clickable", () => (
-    <InterviewerListItem
-      name={interviewer.name}
-      avatar={interviewer.avatar}
-      setInterviewer={() => action("setInterviewer")(interviewer.id)}
+    <InterviewerList
+      interviewers={interviewers}
+      onChange={action("setInterviewer")}
     />
   ));
