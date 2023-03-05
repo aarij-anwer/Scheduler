e85ba2689b9273c27145e7f67f9651165979b9be
Aarij Anwer <aarij.anwer@gmail.com>
2023-03-05 15:36:26 -0500

    add Header to stories

--- src/components/Appointment/Header.js
+++ src/components/Appointment/Header.js
@@ -1,8 +1,13 @@
 import React from "react";
-import "components/Appointment/Header.scss";
+// import "components/Appointment/Header.scss";
 
 const Header = (props) => {
-
+  return (
+    <header className="appointment__time">
+      <h4 className="text--semi-bold">12pm</h4>
+      <hr className="appointment__separator" />
+    </header>
+    );
 };
 
 export default Header;
