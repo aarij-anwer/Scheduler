76304946a88a969814da8d4a9b172b1cd891daa3
Aarij Anwer <aarij.anwer@gmail.com>
2023-03-14 01:23:25 -0400

    add folder for hooks and files for custom hook and test

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,13 @@
+import { useState } from "react"; 
+
+const useVisualMode = (initial) => {
+  const [mode, setMode] = useState(initial);
+
+  const transition = (newMode) => {
+    setMode(newMode);
+  };
+
+  return { mode, transition };
+};
+
+export default useVisualMode;
