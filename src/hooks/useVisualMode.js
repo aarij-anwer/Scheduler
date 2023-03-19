f47b2dc0450c2bf0075d86c8d479881a464ad0c7
Aarij Anwer <aarij.anwer@gmail.com>
2023-03-19 10:51:54 -0300

    add documentation to useVisualMode hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,9 +1,11 @@
 import { useState } from "react"; 
 
+//Custom hook `useVisualMode` will be responsible for setting and maintaining the mode and history of mode for the application. The mode is used to determine what is shown to the user and the history enables the back/cancel functionality.
 const useVisualMode = (initial) => {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]); 
 
+  //sets mode to `newMode` and adds it to history. Deletes the previous mode from the history if `replace = true`
   const transition = (newMode, replace = false) => {
     setMode(newMode);
     if (!replace) {
@@ -18,6 +20,7 @@ const useVisualMode = (initial) => {
     }
   };
 
+  //moves back through the history array, stopping at the initial mode
   const back = () => {
     if (mode !== initial) {
       setMode(history[history.length-2]);
