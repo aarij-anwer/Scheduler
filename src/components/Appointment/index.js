32f757d50a0747ee481527450725020bf03def1e
Aarij Anwer <aarij.anwer@gmail.com>
2023-03-23 17:22:32 -0400

    fix a major bug identified when editing an appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -93,7 +93,7 @@ const Appointment = (props) => {
       {mode === EDIT && (
         <Form
           student={props.interview.student}
-          interviewer={props.interview.interviewer}
+          interviewer={props.interview.interviewer.id}
           interviewers={interviewers}
           onCancel={back}
           onSave={save} />
