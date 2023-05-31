7b6822f30397908e47eca53bb47ecc03d4bd0dd3
Aarij Anwer <94186270+aarij-anwer@users.noreply.github.com>
2023-05-31 13:52:32 -0400

    ensure API is live before loading page

--- src/index.js
+++ src/index.js
@@ -10,4 +10,12 @@ if (process.env.REACT_APP_API_BASE_URL) {
   axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
 }
 
-ReactDOM.render(<Application />, document.getElementById("root"));
+axios.get("/api/days")
+  .then((response) => {
+    console.log("Make sure railway API is live", response);
+    ReactDOM.render(<Application />, document.getElementById("root"));
+  })
+  .catch((error) => {
+    console.error("API call failed:", error);
+    window.location.reload();
+  });
