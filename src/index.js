da95842813c6a74ffd694ec38681b83784f8b150
Aarij Anwer <94186270+aarij-anwer@users.noreply.github.com>
2023-05-31 14:07:35 -0400

    update code so it reloads a few times then errors out

--- src/index.js
+++ src/index.js
@@ -10,12 +10,38 @@ if (process.env.REACT_APP_API_BASE_URL) {
   axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
 }
 
+const MAX_RELOAD_ATTEMPTS = 3;
+
+function reloadIndexJs() {
+  const reloadAttempts = localStorage.getItem("reloadAttempts")
+    ? Number(localStorage.getItem("reloadAttempts"))
+    : 0;
+
+  if (reloadAttempts < MAX_RELOAD_ATTEMPTS) {
+    console.log("Reloading index.js");
+    localStorage.setItem("reloadAttempts", String(reloadAttempts + 1));
+    setTimeout(() => {
+      window.location.reload();
+    }, 1000); // Wait for 1 second before reloading
+  } else {
+    console.error("API could not be loaded after multiple attempts");
+    ReactDOM.render(
+      <div>
+        <h1>API could not be loaded</h1>
+        <p>Please try again later.</p>
+      </div>,
+      document.getElementById("root")
+    );
+  }
+}
+
 axios.get("/api/days")
   .then((response) => {
     console.log("Make sure railway API is live", response);
+    localStorage.removeItem("reloadAttempts");
     ReactDOM.render(<Application />, document.getElementById("root"));
   })
   .catch((error) => {
     console.error("API call failed:", error);
-    window.location.reload();
+    reloadIndexJs();
   });
