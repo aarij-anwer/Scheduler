384310602d9f5717903dc1d216c9b2de198b6b76
Aarij Anwer <94186270+aarij-anwer@users.noreply.github.com>
2023-06-03 20:47:51 -0400

    add better error handling when railway fails

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import "index.scss";
 
 import Application from "components/Application";
 import axios from "axios";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 if (process.env.REACT_APP_API_BASE_URL) {
   axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
@@ -12,10 +14,11 @@ if (process.env.REACT_APP_API_BASE_URL) {
 
 const MAX_RELOAD_ATTEMPTS = 3;
 
-function reloadIndexJs() {
+function reloadIndexJs(num) {
+  ReactDOM.render(<Status message="Establishing connection with the Railway API" />, document.getElementById("root"));
   const reloadAttempts = localStorage.getItem("reloadAttempts")
     ? Number(localStorage.getItem("reloadAttempts"))
-    : 0;
+    : num;
 
   if (reloadAttempts < MAX_RELOAD_ATTEMPTS) {
     console.log("Reloading index.js");
@@ -25,13 +28,9 @@ function reloadIndexJs() {
     }, 1000); // Wait for 1 second before reloading
   } else {
     console.error("API could not be loaded after multiple attempts");
-    ReactDOM.render(
-      <div>
-        <h1>API could not be loaded</h1>
-        <p>Please try again later.</p>
-      </div>,
-      document.getElementById("root")
-    );
+    const message = "Railway API failed to load. It's very frustrating. Please close this message by clicking on the x to try again.";
+    localStorage.removeItem("reloadAttempts");
+    ReactDOM.render(<Error message={message} onClose={() => reloadIndexJs(0)} />, document.getElementById("root"));
   }
 }
 
@@ -43,5 +42,5 @@ axios.get("/api/days")
   })
   .catch((error) => {
     console.error("API call failed:", error);
-    reloadIndexJs();
+    reloadIndexJs(0);
   });
